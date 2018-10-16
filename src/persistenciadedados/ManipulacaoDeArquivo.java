ebf483ac0e9e1170b3ec67ed15f1b18dfafdc122
Aluno <Aluno@SN-279228>
2018-10-16 20:51:09 -0300

    Finalizado classe de manipualção de arquivos

---
+++ src/persistenciadedados/ManipulacaoDeArquivo.java
@@ -0,0 +1,65 @@
+package persistenciadedados;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ManipulacaoDeArquivo {
+
+    private String caminhoArquivo;
+    private String nomeArquivo;
+
+    public ManipulacaoDeArquivo(String caminho, String nome) {
+        this.caminhoArquivo = caminho;
+        this.nomeArquivo = nome;
+    }
+
+    public void gravar(String conteudo) throws IOException {
+        FileWriter arquivoTxt = new FileWriter(this.caminhoArquivo + "/" + this.nomeArquivo);
+        PrintWriter gravarArq = new PrintWriter(arquivoTxt);
+        gravarArq.print(conteudo);
+        arquivoTxt.close();
+    }
+
+    public String[] ler() throws IOException {
+
+        FileReader arquivoTxt = new FileReader(this.caminhoArquivo + "/" + this.nomeArquivo);
+        BufferedReader lerArq = new BufferedReader(arquivoTxt);
+
+        String linha = lerArq.readLine();
+        int contador = 0;
+        while (linha != null) {
+            contador++;
+            linha = lerArq.readLine();
+        }
+
+        String[] conteudo = new String[contador];
+        lerArq.reset();
+        
+        //Popula o array
+        linha = lerArq.readLine();
+        contador = 0;
+        while (linha != null) {
+            conteudo [contador] = linha;
+            contador++;
+            linha = lerArq.readLine();
+        }
+        
+        return conteudo;
+    }
+
+    public String lerEmString() throws IOException {
+        
+        String conteudoArquivo[] = this.ler();
+        String conteudo = "";
+        
+        for (int i = 0; i < conteudoArquivo.length; i++) {
+            conteudo = conteudo + conteudoArquivo[i]+"\n";
+        }
+        
+        return conteudo;
+    }
+
+}
